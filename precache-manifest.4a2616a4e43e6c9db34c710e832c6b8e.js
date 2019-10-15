0b5b21950f173724f0224e25d00cdc7c58f137a4
Chopper <ilua292010@gmail.com>
2019-10-15 19:42:35 +0300

    Updates

---
+++ precache-manifest.4a2616a4e43e6c9db34c710e832c6b8e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccc180c79a1a1451400b24346c722e7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf2d4d57d021ae67b790",
+    "url": "/static/css/main.119bd41b.chunk.css"
+  },
+  {
+    "revision": "9ef6f226bb6df220ea8a",
+    "url": "/static/js/2.3a500b53.chunk.js"
+  },
+  {
+    "revision": "cf2d4d57d021ae67b790",
+    "url": "/static/js/main.cecf327d.chunk.js"
+  },
+  {
+    "revision": "841c4ce45f919e0c7eef",
+    "url": "/static/js/runtime-main.fde79815.js"
+  },
+  {
+    "revision": "cbeb4742adbc6308508b5270571729e3",
+    "url": "/static/media/SegoeLight.cbeb4742.ttf"
+  },
+  {
+    "revision": "fc15cc81e02f5e33593ad4970abde469",
+    "url": "/static/media/SegoeRegular.fc15cc81.ttf"
+  },
+  {
+    "revision": "aec97d0427164d278855d38333b51d2b",
+    "url": "/static/media/cart.aec97d04.png"
+  },
+  {
+    "revision": "eeb911b65da94ff3b518ab71a3d5f4ff",
+    "url": "/static/media/dsfsdfsdfds@3x.eeb911b6.png"
+  },
+  {
+    "revision": "c146354e495398ba830143ef4e827b60",
+    "url": "/static/media/iphone.c146354e.png"
+  }
+]);
